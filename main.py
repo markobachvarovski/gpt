ac24e684432efe7b2de4b719ffd3a70d41f835fd
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-05-23 20:56:39 -0400

    Added generation and optimization step

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ itos = {i: ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]  # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
 
-batch_size = 4
+batch_size = 32
 chunk_size = 8
 
 # Train and test splits
@@ -40,19 +40,49 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, x, y):
+    def forward(self, x, y=None):
         logits = self.token_embedding_table(x)
 
-        logits = logits.view(batch_size*chunk_size, vocab_size)
-        targets = y.view(batch_size*chunk_size)
+        if y is None:
+            loss = None
+        else:
+            logits = logits.view(batch_size * chunk_size, vocab_size)
+            targets = y.view(batch_size * chunk_size)
 
-        loss = F.cross_entropy(logits, targets)
+            loss = F.cross_entropy(logits, targets)
 
         return logits, loss
+
+    def generate(self, current_tokens, max_new_tokens):
+
+        for i in range(max_new_tokens):
+            logits, loss = self.forward(current_tokens)
+            logits = logits[:, -1, :]
+            probabilities = F.softmax(logits, dim=-1)
+            next_token = torch.multinomial(probabilities, num_samples=1)
+            current_tokens = torch.cat((current_tokens, next_token), dim=1)
+
+        return current_tokens
 
 
 xb, yb = get_batch('train')
 
-blm = BigramLanguageModel(vocab_size)
-logits, loss = blm.forward(xb, yb)
-print(loss)
+model = BigramLanguageModel(vocab_size)
+logits_outer, loss_outer = model.forward(xb, yb)
+
+current_tokens = torch.zeros((1, 1), dtype=torch.long)
+max_new_tokens = 100
+print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist()))
+
+# Optimization step
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+for i in range(10000):
+    x_train, y_train = get_batch('train')
+
+    logits, loss = model(x_train, y_train)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist()))
