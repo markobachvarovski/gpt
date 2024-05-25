b9e7a4e3f9d1471750057996a308f5d66e5d26c6
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-05-24 22:37:31 -0400

    Adding loss estimation function

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from torch.nn import functional as F
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = 32
+chunk_size = 8
+epoches = 10000
+learning_rate = 1e-3
+eval_iters = 100
+eval_interval = 1000
+
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
@@ -13,9 +21,6 @@ stoi = {ch: i for i, ch in enumerate(chars)}
 itos = {i: ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]  # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
-
-batch_size = 32
-chunk_size = 8
 
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
@@ -31,8 +36,22 @@ def get_batch(split):
     ix = torch.randint(len(data) - chunk_size, (batch_size,))
     x = torch.stack([data[i:i + chunk_size] for i in ix])
     y = torch.stack([data[i + 1:i + chunk_size + 1] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 class BigramLanguageModel(nn.Module):
 
@@ -67,17 +86,22 @@ class BigramLanguageModel(nn.Module):
 
 xb, yb = get_batch('train')
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel(vocab_size).to(device)
 logits_outer, loss_outer = model.forward(xb, yb)
 
-current_tokens = torch.zeros((1, 1), dtype=torch.long)
+current_tokens = torch.zeros((1, 1), dtype=torch.long, device=device)
 max_new_tokens = 100
-print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist()))
+print("Before optimization:")
+print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist())+"\n")
 
 # Optimization step
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-for i in range(10000):
+for i in range(epoches):
+    if i % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {i}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
     x_train, y_train = get_batch('train')
 
     logits, loss = model(x_train, y_train)
@@ -85,4 +109,5 @@ for i in range(10000):
     loss.backward()
     optimizer.step()
 
+print("After optimization:")
 print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist()))
