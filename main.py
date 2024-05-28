7ad4bc2234468dd203c4bf42e9e802607572f27d
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-05-27 22:49:58 -0400

    Added a self attention head

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ epoches = 10000
 learning_rate = 1e-3
 eval_iters = 100
 eval_interval = 1000
+num_embed_dim = 32
+head_size = 16
 
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
@@ -39,6 +41,7 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
+
 @torch.no_grad()
 def estimate_loss():
     out = {}
@@ -53,14 +56,22 @@ def estimate_loss():
     model.train()
     return out
 
+
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, num_embed_dim)
+        self.position_embedding_table = nn.Embedding(chunk_size, num_embed_dim)
+        self.lm_head = nn.Linear(num_embed_dim, vocab_size)
+        self.self_attention_head = Head(num_embed_dim)
 
     def forward(self, x, y=None):
-        logits = self.token_embedding_table(x)
+        token_embeddings = self.token_embedding_table(x)
+        position_embeddings = self.position_embedding_table(torch.arange(chunk_size, device=device))
+        x = token_embeddings + position_embeddings
+        x = self.self_attention_head(x)
+        logits = self.lm_head(x)
 
         if y is None:
             loss = None
@@ -75,6 +86,7 @@ class BigramLanguageModel(nn.Module):
     def generate(self, current_tokens, max_new_tokens):
 
         for i in range(max_new_tokens):
+            current_tokens = current_tokens[:, -chunk_size:]
             logits, loss = self.forward(current_tokens)
             logits = logits[:, -1, :]
             probabilities = F.softmax(logits, dim=-1)
@@ -84,15 +96,38 @@ class BigramLanguageModel(nn.Module):
         return current_tokens
 
 
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(num_embed_dim, head_size, bias=False)
+        self.query = nn.Linear(num_embed_dim, head_size, bias=False)
+        self.value = nn.Linear(num_embed_dim, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(chunk_size, chunk_size)))
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        k = self.key(x)  # (B,T,hs)
+        q = self.query(x)  # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:chunk_size, :chunk_size] == 0, float('-inf'))  # (B, T, T)
+        wei = F.softmax(wei, dim=-1)  # (B, T, T)
+        # perform the weighted aggregation of the values
+        v = self.value(x)  # (B,T,hs)
+        out = wei @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+
+
 xb, yb = get_batch('train')
 
-model = BigramLanguageModel(vocab_size).to(device)
+model = BigramLanguageModel().to(device)
 logits_outer, loss_outer = model.forward(xb, yb)
 
 current_tokens = torch.zeros((1, 1), dtype=torch.long, device=device)
 max_new_tokens = 100
 print("Before optimization:")
-print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist())+"\n")
+print(decode(model.generate(current_tokens, max_new_tokens)[0].tolist()) + "\n")
 
 # Optimization step
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
